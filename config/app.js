d035951af36e9592a8cb6e013d25f2d148eb7f72
talauiwdiqugdiq <tala.bashanfar@ontariotechu.net>
2024-11-05 20:51:59 -0500

    4th

--- config/app.js
+++ config/app.js
@@ -1,4 +1,4 @@
-/*installed 3rd party packages*/
+/* installed 3rd party packages */
 let createError = require('http-errors');
 let express = require('express');
 let path = require('path');
@@ -12,19 +12,21 @@ let bookRouter = require('../routes/book');
 let app = express();
 let mongoose = require('mongoose');
 let DB = require('./db');
-const { title } = require('process');
-//point my mongoose to the URI
-mongoose.connect(DB.URI);
+
+// point mongoose to the URI
+mongoose.connect(DB.URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 let mongoDB = mongoose.connection;
-mongoDB.on('error', console.error.bind(console,'Connection Error'))
-mongoDB.once('open',()=>{
-  console.log('MongoDB Connected')
-})
-mongoose.connect(DB.URI,{useNewURIParser:true,
-  useUnifiedTopology:true
-})
+mongoDB.on('error', console.error.bind(console, 'Connection Error'));
+mongoDB.once('open', () => {
+  console.log('MongoDB Connected');
+});
+
 // view engine setup
-app.set('../views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
@@ -39,19 +41,19 @@ app.use('/users', usersRouter);
 app.use('/books', bookRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error',{title:'Error'});
+  res.render('error', { title: 'Error' });
 });
 
 module.exports = app;
